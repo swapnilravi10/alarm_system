df4f7c120959d03e505515de6dca5bcfb1ae0cc2
swapnilravi10 <swapnil.ravi10@gmail.com>
2020-03-30 13:46:47 +0000

    Secondary commit

--- conf.py
+++ conf.py
@@ -1,10 +1,10 @@
 #device config
-api_key = '55629bbd-b8f3-42d8-83fc-59d486d6e957'
-device_id = 'BOLT292461'
+api_key = '<your_apiKey>'
+device_id = '<your_deviceId>'
 
 #SMS config
-AUTH_TOKEN = 'e858a1e3cc0e5747eeec7dcac306d3b0'
-SID = 'AC6285fa7c50aecdb42918a8f8e80d56b5'
-FROM_NUMBER = '9089982419'
-TO_NUMBER = '+919004527491'
+AUTH_TOKEN = '<your_authToken>'
+SID = '<your_SID>'
+FROM_NUMBER = '<from_number>'
+TO_NUMBER = '<to_number>'
 

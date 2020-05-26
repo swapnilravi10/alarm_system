ef26f92172007595b851e7c594c968d41f6ace9c
swapnilravi10 <42041670+swapnilravi10@users.noreply.github.com>
2020-05-26 18:27:23 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,14 +1,14 @@
 #device config
-api_key = '55629bbd-b8f3-42d8-83fc-59d486d6e957'
-device_id = 'BOLT292461'
+api_key = ''
+device_id = ''
 
 #SMS config
-AUTH_TOKEN = '46d391f16a3d01a345e145aef1ab4d8c'
-SID = 'AC6285fa7c50aecdb42918a8f8e80d56b5'
+AUTH_TOKEN = ''
+SID = ''
 FROM_NUMBER = '+19089982419'
-TO_NUMBER = '+919004527491'
+TO_NUMBER = ''
 
 # Email you want to send the update from (only works with gmail)
-fromEmail = 'swapnil.ravi10@gmail.com'
-fromEmailPassword = 'Trustmeiamadoctor'
-toEmail = 'swapnil.ravi10@gmail.com'
+fromEmail = ''
+fromEmailPassword = ''
+toEmail = ''
